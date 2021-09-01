ecfc914ceec4cfc6285fb311c238d6d69d1d4f39
Sam'an <samangriffiths47@gmail.com>
2021-09-01 12:13:38 -0500

    worked on nav & search

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,17 @@
+import React from 'react'
+import { BASE_URL } from './globals'
 import './App.css'
 
 function App() {
+  const [posts, setPosts] = useState([])
+  const [searched, toggleSearched] = useState(false)
+  const [searchResults, setSearchResults] = useState([])
+  const [searchQuery, setSearchQuery] = useState('')
+  const [color, setColor] = useState('')
+  const [model, setModel] = useState('')
+  const [brand, setBrand] = useState('')
+  const [request, changeIt] = useState(false)
+
   return <div className="App"></div>
 }
 
