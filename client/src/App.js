9da37b6cb6f761a1a6cfb4fc9e1f0f5d7a1c51b8
Sam'an <samangriffiths47@gmail.com>
2021-09-05 07:45:48 -0500

    add post create function

--- client/src/App.js
+++ client/src/App.js
@@ -12,9 +12,9 @@ import NewPost from './components/NewPost'
 function App() {
   const dropDownDefault = 'Choose One'
   const [posts, setPosts] = useState([])
-  const [request, toggleRequest] = useState(false)
   const [category, changeCategory] = useState(dropDownDefault)
   const [keyword, changeKeyword] = useState(dropDownDefault)
+  const [postRender, setPostRender] = useState(false)
 
   const grabPosts = async () => {
     const res = await axios.get(`${BASE_URL}/posts`)
@@ -29,7 +29,7 @@ function App() {
   }
   useEffect(() => {
     grabPosts()
-  }, [request])
+  }, [postRender])
   useEffect(() => {
     dd()
   }, [category])
@@ -51,7 +51,11 @@ function App() {
           path="/"
           component={(props) => (
             <div className="postDisplay">
-              <NewPost {...props} />
+              <NewPost
+                {...props}
+                postRender={postRender}
+                setPostRender={setPostRender}
+              />
               {posts.map((one) => {
                 return (
                   <section>
--- client/src/components/NewPost.js
+++ client/src/components/NewPost.js
@@ -1,27 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import axios from 'axios'
+import { BASE_URL } from '../globals'
 
-export default function NewPost() {
+export default function NewPost(props) {
+  const [username, setUsername] = useState('')
+  const [language, setLanguage] = useState('')
+  const [skill, setSkill] = useState('')
+  const [description, setDescription] = useState('')
+  const [link, setLink] = useState('')
+
+  const userForm = (e) => {
+    setUsername(e.target.value)
+  }
+  const skillForm = (e) => {
+    setSkill(e.target.value)
+  }
+  const descriptionForm = (e) => {
+    setDescription(e.target.value)
+  }
+
+  const linkForm = (e) => {
+    setLink(e.target.value)
+  }
+  const languageForm = (e) => {
+    setLanguage(e.target.value)
+  }
+  const createPostOnSubmit = async (e) => {
+    e.preventDefault()
+
+    const newPost = {
+      language: `${language.toLowerCase()}`,
+      languageDisplay: `${language}`,
+      descriptionDisplay: `${language}`,
+      description: `${description}`,
+      comments: [],
+      skill: `${skill.toLowerCase()}`,
+      skillDisplay: `${skill}`,
+      user: `${username.toLowerCase()}`,
+      userDisplay: `${username}`,
+      link: `${link}`
+    }
+
+    await axios.post(`${BASE_URL}/c_post`, newPost)
+
+    props.postRender ? props.setPostRender(false) : props.setPostRender(true)
+  }
   return (
     <Form>
       <Form.Group className="mb-3" controlId="userInput">
         <Form.Label>User</Form.Label>
-        <Form.Control type="text" placeholder="Enter UserName" />
+        <Form.Control
+          type="text"
+          placeholder="Enter UserName"
+          onChange={(e) => userForm(e)}
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="languageInput">
         <Form.Label>Relevant Language(s)</Form.Label>
-        <Form.Control type="text" placeholder="Enter Language(s)" />
+        <Form.Control
+          type="text"
+          placeholder="Enter Language(s)"
+          onChange={(e) => languageForm(e)}
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="skillInput">
         <Form.Label>Relevant Skill(s)</Form.Label>
-        <Form.Control type="text" placeholder="Enter Skill(s)" />
+        <Form.Control
+          type="text"
+          placeholder="Enter Skill(s)"
+          onChange={(e) => skillForm(e)}
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="descriptionInput">
         <Form.Label>User Description</Form.Label>
-        <Form.Control type="text" placeholder="Enter Your Description" />
+        <Form.Control
+          type="text"
+          placeholder="Enter Your Description"
+          onChange={(e) => descriptionForm(e)}
+        />
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button
+        variant="primary"
+        type="submit"
+        onSubmit={(e) => {
+          createPostOnSubmit(e)
+        }}
+      >
         Submit
       </Button>
     </Form>
